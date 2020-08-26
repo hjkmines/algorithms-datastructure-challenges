6aebcf3d1172ad76f4bc973fd801f3737666499d
Hyung Kim <hjkmines@gmail.com>
2020-08-25 18:00:46 -0600

    adds third solution

--- index.js
+++ index.js
@@ -17,3 +17,30 @@ function compare(a, b) {
 
 console.log(compare(alice, bob)); 
 
+//Problem 2: Big Sum Calculation 
+const largeNumbers = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+function largeSum(arr) {
+    const sum = arr.reduce((accum, currVal) => accum + currVal); 
+    return sum; 
+}
+
+console.log(largeSum(largeNumbers)); 
+
+
+//Problem 3: Absolute sum of the diagnols 
+function sumCross(arr) {
+    //left to right 
+    let leftToRight = 0; 
+    //right to left 
+    let rightToLeft = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        leftToRight += arr[i][i]
+        rightToLeft += arr[i][arr.length - i - 1]; 
+    }; 
+
+    let sum = Math.abs(leftToRight + rightToLeft); 
+
+    return sum; 
+} 
