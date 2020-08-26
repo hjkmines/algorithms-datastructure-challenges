abe723652f23f7398fdf26f443e7d1c9a6068c44
Hyung Kim <hjkmines@gmail.com>
2020-08-26 11:27:58 -0600

    adds solution to challenge 1

--- algorithms.js
+++ algorithms.js
@@ -114,3 +114,19 @@ console.log(staircase(10));
 
      return count; 
  }
+  
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+ let string = 'hello Tony'
+ function reverseString(str) {
+     //split into a array element for each word 
+     const reversedArr = str.split('').reverse().join('')
+     return reversedArr; 
+ }
+
+ console.log(reverseString(string))
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
