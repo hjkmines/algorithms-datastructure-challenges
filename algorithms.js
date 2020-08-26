a332cf392ad110a08aab7afb5628f606efb64b14
Hyung Kim <hjkmines@gmail.com>
2020-08-26 13:01:52 -0600

    adds 6th solution to challenge

--- algorithms.js
+++ algorithms.js
@@ -177,3 +177,24 @@ function maxCharacter(str) {
 }
 
 console.log(maxCharacter(maxChar)); 
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. 
+//CONTINUED: For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". 
+//CONTINUED: For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+
+function fizzBuzz() {
+    for (let i = 1; i < 101; i++) {
+        if (i%5 === 0 && i%3 === 0){
+            console.log('FizzBuzz')
+        } else if (i%3 === 0) {
+            console.log('Fizz')
+        } else if (i%5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+console.log(fizzBuzz()); 
