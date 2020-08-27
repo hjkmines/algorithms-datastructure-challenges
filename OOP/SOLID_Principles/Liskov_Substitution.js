fbe4722f71b9547dde1a0c0ace21bd60b05ff322
Hyung Kim <hjkmines@gmail.com>
2020-08-27 14:27:24 -0600

    adds liskov principle

---
+++ OOP/SOLID_Principles/Liskov_Substitution.js
@@ -0,0 +1,37 @@
+class FlyingBird {
+    fly() {
+        console.log('I can fly')
+    }
+}
+
+class SwimmingBird {
+    swim() {
+        console.log('I can swim')
+    }
+}
+
+class Duck extends FlyingBird {
+    quack() {
+        console.log('I can quack')
+    }
+}
+
+class Penguin extends SwimmingBird {
+    swim() {
+        console.log('I can swim')
+    }
+}
+
+function makeBirdFly(bird) {
+    bird.fly();  
+} 
+
+function makeBirdSwim(bird) {
+    bird.swim(); 
+}
+
+const duck = new Duck; 
+const penguin = new Penguin; 
+
+makeBirdFly(duck); 
+makeBirdSwim(penguin); 
