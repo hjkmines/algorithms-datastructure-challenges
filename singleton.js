fc67e9c9fa74b0710dba77167f2dec9c7132cab6
Hyung Kim <hjkmines@gmail.com>
2020-08-27 11:44:44 -0600

    completes solution to singleton pattern

--- singleton.js
+++ singleton.js
@@ -6,9 +6,20 @@ let obj = (function () {
 
     //creates the objInstance if one isn't available 
     function create() {
-
+        let _isRunning = false; 
+        let start = function() {
+            _isRunning = true; 
+        }
+        let stop = function() {
+            _isRunning = false; 
+        }
+        let currentState = function() {
+            return _isRunning
+        }
         return {
-
+            start: start, 
+            stop: stop,
+            currentState: currentState
         }
     }
 
@@ -22,3 +33,6 @@ let obj = (function () {
         }
     }
 })()
+
+let obj1 = obj.getInstance(); 
+let obj2 = obj.getInstance(); 
