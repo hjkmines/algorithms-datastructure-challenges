36d4cad9bfc2d7085f59b9b5eebccc9c5f78dab0
Hyung Kim <hjkmines@gmail.com>
2020-08-26 22:53:02 -0600

    adds second solution to deck creation

--- dataStructures.js
+++ dataStructures.js
@@ -47,3 +47,43 @@ let deck = new Deck();
 deck.createDeck(suits, values); 
 deck.shuffle();
 console.log(deck.deal());  
+
+
+//Method 2
+class Deck {
+    constructor() {
+      this.deck = [];
+  
+      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+      const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  
+      for (let suit in suits) {
+        for (let value in values) {
+          this.deck.push(`${values[value]} of ${suits[suit]}`);
+        }
+      }
+    }
+  
+    shuffle(){
+      const { deck } = this;
+      let m = deck.length, i;
+  
+      while(m){
+        i = Math.floor(Math.random() * m--);
+  
+        [deck[m], deck[i]] = [deck[i], deck[m]];
+      }
+  
+      return this;
+    }
+  
+    deal(){
+      return this.deck.pop();
+    }
+  }
+  
+  const deck1 = new Deck();
+  deck1.shuffle()
+  console.log(deck1.deck);
+  deck1.deal()
+  console.log(deck1.deck);
