3e8f159955e428326676e3153f4f1099e8cfb059
Hyung Kim <hjkmines@gmail.com>
2020-08-27 11:31:54 -0600

    adds solution to singleton pattern

--- singleton.js
+++ singleton.js
@@ -1,1 +1,31 @@
 //Singleton pattern 
+class Process {
+    constructor(state) {
+        this.state = state; 
+    }
+}
+
+const Singleton = (function() {
+    function ProcessManager() {
+        this.numProcess = 0; 
+    }
+
+    let pManager 
+
+    function createProcessManager() {
+        pManager = new ProcessManager()
+        return pManager; 
+    }
+
+    return {
+        getProcessManager:() => {
+            if(!pManager)
+                pManager = createProcessManager(); 
+                return pManager
+        }
+    }
+})()
+
+const processManager = Singleton.getProcessManager()
+const processManager2 = Singleton.getProcessManager()
+console.log(processManager === processManager2); 
