4855e98c769febab8758fdfdfb0c40dc8f899f9e
Hyung Kim <hjkmines@gmail.com>
2020-08-27 20:50:53 -0600

    adds constructor pattern and dependency inversion solution

---
+++ JavaScript_Patterns/constructor.js
@@ -0,0 +1,9 @@
+// This creates a new empty Object
+
+var newObject = {};
+
+// This creates a new empty Object
+
+var newObject = Object.create(Object.prototype);
+
+var newObject = newObject();
--- OOP/SOLID_Principles/Dependency_Inversion_Principle.js
+++ OOP/SOLID_Principles/Dependency_Inversion_Principle.js
@@ -1,1 +1,53 @@
-//Dependecy inversion: 
+//Dependecy inversion: your high level code should not depend on low level implementation. 
+//Create a intermediate wrapper API
+//For example, store (high level code) => payment processor <= Paypal (low level)
+
+class Store { //this high level does not need to be modified since we now have wrappers for the apis to make modifications.
+    constructor (paymentProccessor) {
+        this.paymentProccessor = paymentProccessor  //middle wrapper between store and the two APIs
+    }
+
+    purchaseBike(quantity) {
+        this.paymentProccessor.pay(200 * quantity)
+    }
+}
+
+class StripePaymentProcessor { //wrapper 1 for the Stripe pattern 
+    constructor(user) {
+        this.stripe = new Stripe(user)  
+    }
+
+    pay(amountInDollars) {
+        this.stripe.makePayment(amountInDollars * 100)
+    }
+}
+
+class PaypalPaymentProcessor { //wrapper 2 for the paypal pattern 
+    constructor(user) {
+        this.user = user 
+        this.paypal = new Paypal()
+    }
+
+    pay(amountInDollars) {
+        this.stripe.makePayment(this.user, amountInDollars)
+    }
+}
+
+class Stripe {  //API number one 
+    constructor(user) {
+        this.user = user 
+    }
+
+    makePayment(amountInCents) {
+        console.log(`${this.user} made a payment of $${amountInCents / 100}`)
+    }
+}
+
+class Paypal { //API number two 
+    makePayment(user, amountInDollars) {
+        console.log(`${user} made payment of $${amountInDollars} with Paypal`)
+    }
+}
+
+const store = new Store(new StripePaymentProcessor('Tony'))
+store.purchaseBike(2); 
