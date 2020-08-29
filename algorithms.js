31b7e155c01a243ecc53ee44858ab122bb4c43e1
Hyung Kim <hjkmines@gmail.com>
2020-08-29 15:23:53 -0600

    adds solution to challenege 14

--- algorithms.js
+++ algorithms.js
@@ -304,3 +304,28 @@ console.log(output);
 function addAll(...numbers) {
     return numbers.reduce((accu, curr) => accu + curr); 
   }
+
+// CHALLENGE 14: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex. sumAllPrimes(10) == 17
+function sumAllPrimes(num) {
+    let total = 0;
+  
+    function checkForPrime(i) {
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    for (let i = 2; i <= num; i++) {
+      if (checkForPrime(i)) {
+        total += i;
+      }
+    }
+    return total;
+  }
+
+  console.log(sumAllPrimes(10)); 
