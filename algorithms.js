73588ab8c2b489a3b74e362d9425ea7e084f73b3
Hyung Kim <hjkmines@gmail.com>
2020-08-29 15:18:51 -0600

    adds solution to challenge 12;

--- algorithms.js
+++ algorithms.js
@@ -274,3 +274,23 @@ function isAnagram(str1, str2) {
 
 console.log(isAnagram('bob', 'joe')); 
 console.log(isAnagram('hello', 'elloh')); 
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+function letterChanges(str) {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, char => {
+        if (char === 'z' || char === 'Z') {
+          return 'a';
+        } else {
+          return String.fromCharCode(char.charCodeAt() + 1);
+        }
+      });
+    
+      newStr = newStr.replace(/a|e|i|o|u/gi, vowel => vowel.toUpperCase());
+    
+      return newStr;
+}
+
+console.log(letterChanges('hello there')); 
