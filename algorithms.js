579ad0fda27a4d516288feac5b0f76b2e02214de
Hyung Kim <hjkmines@gmail.com>
2020-08-29 15:25:16 -0600

    adds alternative solution to challenege 15

--- algorithms.js
+++ algorithms.js
@@ -333,13 +333,7 @@ function sumAllPrimes(num) {
 // CHALLENGE 15: SEEK & DESTROY
 // Remove from the array whatever is in the following arguments. Return the leftover values in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
-function seekAndDestroy(arr) {
-    const args = Array.from(arguments);
-  
-    function filterArr(arr) {
-      // Return true if NOT in array
-      return args.indexOf(arr) === -1;
-    }
-  
-    return arr.filter(filterArr);
+function seekAndDestroy(arr, ...rest) {
+    return arr.filter(val => !rest.includes(val));
   }
+
