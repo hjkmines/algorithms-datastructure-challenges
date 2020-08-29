ed02a6b2d765fc8c663ebec17d3df089aaeb03c5
Hyung Kim <hjkmines@gmail.com>
2020-08-29 15:01:42 -0600

    adds solution to challenge 10

--- algorithms.js
+++ algorithms.js
@@ -248,3 +248,12 @@ function chunkArray(arr, len) {
 }
 
 console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3)); 
+
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+function flattenArray(arrays) {
+    return arrays.flat(); 
+}
+
+console.log(flattenArray([[1, 2, 3],[4, 5, 6],[7]])); 
