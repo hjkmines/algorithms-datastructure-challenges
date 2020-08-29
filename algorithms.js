121a32a67e28f6243f40f99a901df78e3d2ec8ae
Hyung Kim <hjkmines@gmail.com>
2020-08-29 15:26:26 -0600

    adds solution to challenge 17

--- algorithms.js
+++ algorithms.js
@@ -355,3 +355,26 @@ function sortByHeight(a) {
     return sortArr;
   }
   
+
+// CHALLENGE 16: MISSING LETTERS
+// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+// ex.
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters(str) {
+    let compare = str.charCodeAt(0);
+    let missing;
+  
+    str.split('').map((char, i) => {
+      if (str.charCodeAt(i) == compare) {
+        ++compare;
+      } else {
+        missing = String.fromCharCode(compare);
+      }
+    });
+  
+    return missing;
+  }
+  
