765c7f7c82091e4728c0e8ef8f1a73cce110a471
Hyung Kim <hjkmines@gmail.com>
2020-09-04 21:57:39 -0600

    adds solution 4

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -1,9 +1,9 @@
-//What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?
+// Q1. What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?
 var bar = null;
 console.log(typeof bar === "object");  // logs true!
 console.log((bar !== null) && (typeof bar === "object"));  // logs false
 
-//What will the code below output to the console and why?
+// Q2.  What will the code below output to the console and why?
 (function(){
     var a = b = 3;
 })();
@@ -15,7 +15,7 @@ console.log("b defined? " + (typeof b !== 'undefined'));
 a defined? false
 b defined? true
 
-//What will the code below output to the console and why?
+// Q3. What will the code below output to the console and why?
 var myObject = {
     foo: "bar",
     func: function() {
@@ -35,3 +35,11 @@ outer func:  this.foo = bar
 outer func:  self.foo = bar
 inner func:  this.foo = undefined
 inner func:  self.foo = bar
+
+// Q4. What is the significance of, and reason for, wrapping the entire content of a JavaScript source file in a function block?
+// This is an increasingly common practice, employed by many popular JavaScript libraries (jQuery, Node.js, etc.). This technique creates a closure around the entire contents of the file which, perhaps most importantly, creates a private namespace and thereby helps avoid potential name clashes between different JavaScript modules and libraries.
+
+// Another feature of this technique is to allow for an easily referenceable (presumably shorter) alias for a global variable. This is often used, for example, in jQuery plugins. jQuery allows you to disable the $ reference to the jQuery namespace, using jQuery.noConflict(). If this has been done, your code can still use $ employing this closure technique, as follows:
+
+(function($) { /* jQuery plugin code referencing $ */ } )(jQuery);
+
