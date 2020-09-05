09aba0ffa1d0b8d648bc4c074144bedea0f34b17
Hyung Kim <hjkmines@gmail.com>
2020-09-04 21:56:18 -0600

    adds solution to questions 2 and 3

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -6,7 +6,32 @@ console.log((bar !== null) && (typeof bar === "object"));  // logs false
 //What will the code below output to the console and why?
 (function(){
     var a = b = 3;
-  })();
-  
-  console.log("a defined? " + (typeof a !== 'undefined'));
-  console.log("b defined? " + (typeof b !== 'undefined'));
+})();
+
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
+
+//solution: 
+a defined? false
+b defined? true
+
+//What will the code below output to the console and why?
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
+
+//solution: 
+outer func:  this.foo = bar
+outer func:  self.foo = bar
+inner func:  this.foo = undefined
+inner func:  self.foo = bar
