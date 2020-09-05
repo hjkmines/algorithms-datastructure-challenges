3243c7033ac75c8d5adc67c3a54b50931e6a9eaa
Hyung Kim <hjkmines@gmail.com>
2020-09-04 22:03:55 -0600

    adds solution 10

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -118,4 +118,34 @@ function sum(x, y) {
     }
   }
 
-  
+ // Q10. Consider the following code snippet:
+//(a) What gets logged to the console when the user clicks on “Button 4” and why?
+
+// (b) Provide one or more alternate implementations that will work as expected.
+
+ for (var i = 0; i < 5; i++) {
+    var btn = document.createElement('button');
+    btn.appendChild(document.createTextNode('Button ' + i));
+    btn.addEventListener('click', function(){ console.log(i); });
+    document.body.appendChild(btn);
+  }
+
+  //Solution: 
+  for (var i = 0; i < 5; i++) {
+    var btn = document.createElement('button');
+    btn.appendChild(document.createTextNode('Button ' + i));
+    btn.addEventListener('click', (function(i) {
+      return function() { console.log(i); };
+    })(i));
+    document.body.appendChild(btn);
+  }
+
+  //also works: 
+  for (var i = 0; i < 5; i++) {
+    var btn = document.createElement('button');
+    btn.appendChild(document.createTextNode('Button ' + i));
+    (function (i) {
+      btn.addEventListener('click', function() { console.log(i); });
+    })(i);
+    document.body.appendChild(btn);
+  }
