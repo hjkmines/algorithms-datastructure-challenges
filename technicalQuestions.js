2a5171dae8852f1ab4e26379b5774712fc4ae3fa
Hyung Kim <hjkmines@gmail.com>
2020-09-05 23:33:18 -0600

    adsd question 11 and solution

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -149,3 +149,29 @@ function sum(x, y) {
     })(i);
     document.body.appendChild(btn);
   }
+
+  //What will the following code output and why?
+  var b = 1;
+  function outer(){
+       var b = 2
+      function inner(){
+          b++;
+          var b = 3;
+          console.log(b)
+      }
+      inner();
+  }
+  outer();
+
+
+  //Solution: 
+  function inner () {
+    var b; // b is undefined
+    b++; // b is NaN
+    b = 3; // b is 3
+    console.log(b); // output "3"
+}
+
+//explaination: Output to the console will be “3”.
+
+// There are three closures in the example, each with it’s own var b declaration. When a variable is invoked closures will be checked in order from local to global until an instance is found. Since the inner closure has a b variable of its own, that is what will be output.
