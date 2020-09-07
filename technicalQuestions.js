05c424a350b4f2c9e060082337c416b84961b9e7
Hyung Kim <hjkmines@gmail.com>
2020-09-06 23:18:52 -0600

    adds solution 15

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -240,3 +240,41 @@ console.log(a[b]);
 The output of this code will be 456 (not 123).
 
 // The reason for this is as follows: When setting an object property, JavaScript will implicitly stringify the parameter value. In this case, since b and c are both objects, they will both be converted to "[object Object]". As a result, a[b] anda[c] are both equivalent to a["[object Object]"] and can be used interchangeably. Therefore, setting or referencing a[c] is precisely the same as setting or referencing a[b].
+
+//Write a JavaScript function to find longest substring in a given a string without repeating characters.
+
+//Solution 
+function longest_substring_without_repeating_characters(input) {
+  var chars = input.split('');
+  var curr_char;
+  var str = "";
+  var longest_string = "";
+  var hash = {};
+  for (var i = 0; i < chars.length; i++) {
+  curr_char = chars[i];
+  if (!hash[chars[i]]) 
+  { 
+  str += curr_char; 
+  hash[chars[i]] = {index:i};
+  }
+  else 
+  {
+  if(longest_string.length <= str.length)
+  {
+  longest_string = str;
+  }
+  var prev_dupeIndex = hash[curr_char].index;
+  var str_FromPrevDupe = input.substring(prev_dupeIndex + 1, i);
+  str = str_FromPrevDupe + curr_char;
+  hash = {};
+  for (var j = prev_dupeIndex + 1; j <= i; j++) {
+  hash[input.charAt(j)] = {index:j};
+  }
+  }
+  }
+  return longest_string.length > str.length ? longest_string : str;
+  }
+  console.log(longest_substring_without_repeating_characters("google.com")); 
+  
+  console.log(longest_substring_without_repeating_characters("example.com")); 
+  
