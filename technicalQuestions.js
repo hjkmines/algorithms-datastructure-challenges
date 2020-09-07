6b9a67e8ccc6a37ab85547fbd2243e6175c3aeb7
Hyung Kim <hjkmines@gmail.com>
2020-09-06 23:20:32 -0600

    adds solution 16

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -278,3 +278,21 @@ function longest_substring_without_repeating_characters(input) {
   
   console.log(longest_substring_without_repeating_characters("example.com")); 
   
+
+  //Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string.
+
+  //Solution: 
+  function char_count(str, letter) 
+{
+ var letter_Count = 0;
+ for (var position = 0; position < str.length; position++) 
+ {
+    if (str.charAt(position) == letter) 
+      {
+      letter_Count += 1;
+      }
+  }
+  return letter_Count;
+}
+
+console.log(char_count('w3resource.com', 'o'));
