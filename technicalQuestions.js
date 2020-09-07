692b4aec2a962db6a7a7cc7f6422fc14326df683
Hyung Kim <hjkmines@gmail.com>
2020-09-06 23:21:24 -0600

    adds solution 17 to binary search

--- technicalQuestions.js
+++ technicalQuestions.js
@@ -296,3 +296,39 @@ function longest_substring_without_repeating_characters(input) {
 }
 
 console.log(char_count('w3resource.com', 'o'));
+
+
+// Write a function for searching JavaScript arrays with a binary search.
+
+// Note : A binary search searches by splitting an array into smaller and smaller chunks until it finds the desired value.
+
+//Solution: 
+function array_binarySearch(narray, delement) {
+  var mposition = Math.floor(narray.length / 2);
+
+  if (narray[mposition] === delement){
+     return mposition;
+  }
+  else if (narray.length === 1) 
+  {
+     return null;
+  }
+  else if (narray[mposition] < delement) {
+     var arr = narray.slice(mposition + 1);
+     var res = array_binarySearch(arr, delement);
+     if (res === null)
+     {
+        return null;
+     }
+     else {
+        return mposition + 1 + res;
+     }
+  }
+  else {
+     var arr1 = narray.slice(0, mposition);
+     return array_binarySearch(arr1, delement);
+  }
+}
+
+var myArray = [1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23];
+console.log(array_binarySearch(myArray, 6));
